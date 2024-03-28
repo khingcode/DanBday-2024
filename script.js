0e3763330b175136eb71f66cdfb19b11b88a9d2a
khingcode <khingissupreme@gmail.com>
2024-03-28 19:21:27 +0800

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.button');
+        button.addEventListener('click', () => {
+            document.body.classList.toggle('hide-gradient');
+            document.getElementById("button").style.display ="none";
+        });
+        
