5de0ee6c1f8fc10d4dfcd688e1c2cfc1c7a9f7fd
khingcode <khingissupreme@gmail.com>
2024-04-08 19:25:19 +0800

    Almost done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('.button');
+const button = document.querySelector('.on');
         button.addEventListener('click', () => {
             document.body.classList.toggle('hide-gradient');
             document.getElementById("button").style.display ="none";
